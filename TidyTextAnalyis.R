d7e635a0b31080fbe71c8326a9a524a76accc8d9
Clifton Best <clifton.best@me.com>
2018-08-19 22:11:44 -0400

    Modified for functional ngrams

--- TidyTextAnalyis.R
+++ TidyTextAnalyis.R
@@ -2,6 +2,13 @@ library(dplyr)
 library(readr)
 library(tidytext)
 
+main <- function()
+{
+normalized_descriptions <<- export_description_norm()
+sorted_1_grams <<- tidy_format_process(normalized_descriptions)
+sorted_tri_grams <<- tidy_format_process_trigram(normalized_descriptions)
+}
+
 export_description_norm <- function()
 {
   df <- read_csv("OS3_FY17_Prices_Paid_Data_05172017.csv")
@@ -16,7 +23,7 @@ export_description_norm <- function()
 tidy_format_process <- function(norm_desc)
 {
  text_df <- data_frame(line = 1:nrow(norm_desc), text = norm_desc$PROD_DESC_BY_VENDOR_WS)
- text_df %>%
+ text_df <- text_df %>%
    unnest_tokens(word, text)
  
  data("stop_words")
@@ -29,8 +36,8 @@ sorted_text_df <- text_df %>% count(word, sort = TRUE)
 tidy_format_process_trigram <- function(norm_desc)
 {
   text_df_bigram <- data_frame(line = 1:nrow(norm_desc), text = norm_desc$PROD_DESC_BY_VENDOR_WS)
-  text_df_bigram %>%
-    unnest_tokens(bigram, text, token = "ngrams", n = 3)
+  text_df_bigram <- text_df_bigram %>%
+    unnest_tokens(trigram, text, token = "ngrams", n = 3)
     
-  sorted_text_df <- text_df %>% count(word, sort = TRUE)
+  sorted_text_df <- text_df_bigram %>% count(trigram, sort = TRUE)
 }
